4d8d6f8d45c588327159206ce241ad27bf92b18b
subbareddy-devops <subbareddy.d@outlook.com>
2024-07-23 17:51:01 +0530

    first vaiable shell script

---
+++ Variables1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:, Hi $PERSON2, Good Morning"
+echo "$PERSON2:, Hi $PERSON1, Good Morning"
+echo "$PERSON1:, Hi $PERSON2, How are you"
+echo "$PERSON2:, Hi $PERSON1, I am good. How are you"
