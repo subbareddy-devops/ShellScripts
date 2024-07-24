fabf0966864d9a97698b6c66a8633444805d6a26
subbareddy-devops <subbareddy.d@outlook.com>
2024-07-24 16:21:22 +0530

    Functions script

---
+++ functions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+Date=$(date +%F)
+ScriptName=$0
+LogsDir=/tmp/
+LogFile=$LogsDir/$0-$Date.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+ID=$(id -u)
+
+validate()
+{
+    if [ $1 -ne 0]
+    then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R You are not authorized to perform this action $N" &>>$LogFile
+    exit 1
+else
+    for i in $@
+    do
+        yum install $i -y &>>$LogFile
+        validate $? "$i"
+    done
+fi
+    
